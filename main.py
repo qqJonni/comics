62b883bff36073039912b5e9f958589e6e98e96c
Valeriy Lvov <tepliy_shov@mail.ru>
2023-12-04 02:40:31 +0400

    добавил функцию get_comment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 def download_image():
@@ -11,5 +12,15 @@ def download_image():
         file.write(response.content)
 
 
+def get_comment():
+    url = 'https://xkcd.com/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    json_response = json.loads(response.text)
+    comment = json_response.get('alt')
+    print(comment)
+
+
 if __name__ == '__main__':
     download_image()
+    get_comment()
